b2958979e44a02f8fe51ce7a1b00bad857b8468f
H4b4sch <roman_mueller_holziken@bluewin.ch>
2014-09-02 13:41:31 +0200

    Revert "Added Zeus & Removed empty vehicle despawn"
    This reverts commit 60470f191307b8be1146f8b8c463c76ed6565aa3.

--- swiss_cti.Stratis/Rsc/Game.hpp
+++ swiss_cti.Stratis/Rsc/Game.hpp
@@ -4,15 +4,15 @@ respawnDelay = 3;
 respawnDialog = false;
 
 //--- Require briefing.html to show up.
-onLoadMission = "Swiss CTI (BECTI 0.97) - Stratis";
+onLoadMission = "BECTI 0.97 - Stratis";
 onLoadMissionTime = false;
-onLoadName = "Swiss CTI (BECTI 0.97)";
+onLoadName = "BECTI 0.97";
 
-briefingName = "Swiss CTI (BECTI 0.97)";
+briefingName = "BECTI 0.97";
 
 //--- ArmA 3 Specifics
-dev = "H4b4sch";
-author = "H4b4sch (modified BECTI by Benny)";
+dev = "Benny";
+author = "Benny";
 // overviewPicture
 
 //--- Properties.
